75ee342404a4b082170145b80d82a01bdf9cd3f5
olivierolivier <ot@oliviertalbot.works>
2021-03-04 15:29:54 +0100

    Update page.js

--- back/schemas/documents/page.js
+++ back/schemas/documents/page.js
@@ -23,7 +23,7 @@ export default {
     {
       name: "locale",
       type: "string",
-      hidden: true,
+      readOnly: true,
     },
     {
       name: "title",
@@ -34,36 +34,66 @@ export default {
       name: "slug",
       type: "slug",
       title: "Slug",
-      description:
-        "Some frontends will require a slug to be set to be able to show the post",
-      options: {
-        source: "title",
-        maxLength: 96,
-      },
-    },
-    {
-      name: "template",
-      type: "array",
-      max: 1,
-      of: [{ type: "string" }],
-      validation: (Rule) => Rule.unique(),
-      options: {
-        list: [{ title: "Home", value: "home" }],
-      },
+      description: "Au plus court, sans connecteurs logiques",
     },
     {
       name: "seo",
-      type: "seo",
+      type: "object",
+      title: "SEO",
+      collapsible: "true",
+      fields: [
+        {
+          name: "titleTag",
+          type: "string",
+          title: "Title Tag",
+          description: "Description courte et précise de la page, écrire pour les utilisateurs",
+          validation: Rule => Rule.max(45).warning(`Pas plus de 60 caractères dans le titre de la page.`)
+        },
+        {
+          name: "metaDescription",
+          type: "text",
+          title: "Meta description",
+          description: "Description libre de la page pour informer les utilisateurs et faire marque",
+          validation: Rule => Rule.min(50).max(160).warning(`Viser entre 50 et 160 caractères.`),
+          rows : "4"
+        },
+        {
+          name: "posterImage",
+          type: "image",
+          title: "Poster image",
+          description: "Taille recommandée : 1200×628",
+        },
+      ]
     },
     {
-      name: "texte",
-      type: "portableText",
-      title: "Texte",
+      name: "home",
+      type: "boolean",
+      title: "Homepage",
+      description: "Cocher si cette page est la home du site",
+      layout : "checkbox"
     },
     {
-      name: "liste",
-      type: "array",
-      of: [{ type: "titleTexte" }],
+      name: 'text',
+      type: 'array', 
+      title: 'Texte', 
+    },
+    {
+      name: "projectList",
+      type: "object",
+      title: "Liste de réalisations",
+      fields: [
+       {
+         name: "listTitle",
+         type: "string",
+         title: "Titre de la liste"
+       },
+       {
+         name: "listedProjects",
+         type: "reference",
+         title: "Réalisations listées"
+         to: [{type: 'project'}]
+        },
+      ]
     },
   ],
 };
