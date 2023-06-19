2638c3e590a50ec7679a9446d1b9c28fd1e9d290
Quentin Creuzet <quentin@f451.faith>
2023-06-19 14:45:08 +0200

    Collaborator bug

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -67,7 +67,6 @@ const Header = ({ direction }) => {
   const _menuClose = () => setSmMenuActive(false)
 
   const hasMenu = nav.length > 0
-  // console.log(nav)
   return (
     <header className={clsx(direction, smMenuActive ? "is-active" : "")}>
       <div className="row no-gutter">
